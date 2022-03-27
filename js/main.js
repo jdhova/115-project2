4ceb6f46d6f1f8ea923c738f1c4ffeadb9e51dfd
Jude Okagu <juud0010@gmail.com>
2022-03-26 21:36:11 -0400

    fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 
-   
-
-function verifyPassword(){
-   
+   function verifyPassword(){
+    let pw1 = document.getElementById("password").value;
     let pw2 = document.getElementById("password2").value;
 
-    if(pw!==pw2){
+    if(pw1!==pw2){
         alert("password not matching")
     }
 }

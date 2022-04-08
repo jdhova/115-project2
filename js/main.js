649bb0a36bb6af4cd787325ac3f6b40df548c73b
Saad Siddiqui <saadsiddiqui440@gmail.com>
2022-04-08 00:57:59 -0400

    defining constants

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+
+const form = document.querySelector("form")
+const username = document.getElementById("username")
+
+const password = document.getElementById("password")
+const confirmPassword = document.getElementById("password2")
+const error = document.querySelector("form-control small")
+const success = document.querySelector("form-control small2")
+
 function verifyPassword(){
     let pw = document.getElementById("password").value;  
     let pw2 = document.getElementById("password2").value;  
